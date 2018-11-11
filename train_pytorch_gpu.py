b8b8484e981914be24ab4c0565eabcec11ca0fa9
SunMyeong Lee <actumn814@gmail.com>
2018-11-11 21:50:17 +0900

    Add implementation pytorch_gpu

--- train_pytorch_gpu.py
+++ train_pytorch_gpu.py
@@ -3,12 +3,12 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/game")
 from main import Main
 import numpy as np
-from collections import deque
+from collections import deque, namedtuple
 from skimage import color, transform, exposure
 import random
 
 game = Main()
-D = deque()
+queue = deque()
 from config import *
 
 import torch
@@ -17,7 +17,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda')
 
 
 class DQN(nn.Module):
@@ -30,68 +30,103 @@ class DQN(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, kernel_size=2, stride=2)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=1, padding=(2 - 1) // 2)
 
-        self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(448, 2)
+        self.head = nn.Linear(512, num_of_actions)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.pool2(x)
         return self.head(x.view(x.size(0), -1))
 
 
+def transform_state(single_state):
+    # Turn our screenshot to gray scale, resize to num_of_cols*num_of_rows, and make pixels in 0-255 range
+    single_state = color.rgb2gray(single_state)
+    single_state = transform.resize(single_state, (num_of_cols, num_of_rows))
+    single_state = exposure.rescale_intensity(single_state, out_range=(0, 255))
+    single_state = torch.from_numpy(single_state)
+    single_state = single_state.unsqueeze(0).unsqueeze(0)
+    single_state = single_state.to(device, dtype=torch.float)
+    return single_state
 
+
+model = DQN().to(device)
+model = nn.DataParallel(model)
+#model.load_state_dict(torch.load('./weights_pytorch'))
+optimizer = optim.Adam(model.parameters())
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'is_terminal'))
 
 # Obtain the starting state
-r_0, s_t, s_f = game.MainLoop(3)
-# Failsafe press of x - sometimes startup lags affects ability to enter the game successfully
-# pyautogui.press('x')
-# Turn our screenshot to gray scale, resize to num_of_cols*num_of_rows, and make pixels in 0-255 range
-s_t = color.rgb2gray(s_t)
-s_t = transform.resize(s_t, (num_of_cols, num_of_rows))
-s_t = exposure.rescale_intensity(s_t, out_range=(0, 255))
-s_t = np.stack((s_t, s_t, s_t, s_t), axis=2)
-# In Keras, need to reshape
-s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])  # 1*num_of_cols*num_of_rows*4
+_, curr_state, _ = game.MainLoop(3)
+# Fail safe press of x - sometimes startup lags affects ability to enter the game successfully
+curr_state = transform_state(curr_state)
+curr_state = torch.cat((curr_state, curr_state, curr_state, curr_state), 1)  # 4 image stack
 
-t = 0
-
+steps = 0
 while True:
-    # pyautogui.keyDown('x')
-    # pyautogui.keyUp('left')
-    # pyautogui.keyUp('right')
-    explored = False
-
     loss = 0  # initialize the loss of the network
-    Q_sa = 0  # initialize state
-    action_index = 0  # initialize action index
-    r_t = 0  # initialize reward
-    a_t = np.zeros([num_of_actions])  # initalize acctions as an array that holds one array [0, 0]
+    action = 0  # initialize action index
+    Q_sa = [0]  # initialize state
 
     # choose an action epsilon greedy, or the action that will return the highest reward using our network
     # i chose to create an arbitrary policy before it starts learning to try and explore as much as it can
-    if t < observe:
-        action_index = 1 if random.random() < 0.5 else 0
+    if steps < observe:
+        action = 1 if random.random() < 0.5 else 0
     else:
-        action_index = random.randint(0, num_of_actions - 1)  # choose a random action
-        explored = True
-    # pyautogui.keyDown(action_array[action_index])
-    # keyboard.press(action_array[action_index])
+        if random.random() <= epsilon:
+            action = random.randint(0, num_of_actions - 1)  # choose a random action
+        else:
+            q_index = model(curr_state).max(1)[1]  # input a stack of 4 images, get the prediction
+            action = q_index.item()
 
     # execute the action and observe the reward and the state transitioned to as a result of our action
+    reward, next_state, is_terminal = game.MainLoop(action)
 
-    r_t, s_t1, terminal = game.MainLoop(action_index)
+    # get and pre-process our transitioned state
+    next_state = transform_state(next_state)
+    next_state = torch.cat((next_state, curr_state[:, :3]), 1)  # retain 4 image series
 
-    # get and preprocess our transitioned state
-    print(s_t1.shape)
-    s_t1 = color.rgb2gray(s_t1)
-    print(s_t1.shape)
-    s_t1 = transform.resize(s_t1, (num_of_rows, num_of_cols))
-    print(s_t1.shape)
-    s_t1 = exposure.rescale_intensity(s_t1, out_range=(0, 255))
-    print(s_t1.shape)
+    '''
+    We need enough states in our experience replay deque so that we can take a random sample from it of the size we declared.
+    Therefore we wait until a certain number and observe the environment until we're ready.
+    '''
+    if steps > observe:
+        # sample a random minibatch of transitions in D (replay memory)
+        transitions = random.sample(queue, batch_size)
+        batch = Transition(*zip(*transitions))
 
-    s_t1 = s_t1.reshape(1, s_t1.shape[0], s_t1.shape[1], 1)  # 1x80x80x1
-    print(s_t1.shape)
-    s_t1 = np.append(s_t1, s_t[:, :, :, :3], axis=3)
-    print(s_t1.shape)
+        # Begin creating the input required for the network:
+        # Inputs are our states, outputs/targets are the Q values for those states
+        # we have 32 images per batch, images of 128x128 and 4 of each of these images.
+        # 32 Q values for these batches
+        inputs = torch.cat(batch.state)
+        targets = model(inputs).max(1)  # 32, 2
+        targets = torch.cat((targets[0].reshape(-1, 1), targets[1].reshape(-1, 1).to(device, dtype=torch.float)), 1)
+
+        input_next_states = torch.cat(batch.next_state)
+        Q_sa = model(input_next_states).max(1)[0]
+        for i in range(batch_size):
+            targets[i, batch.action[i]] = batch.reward[i] + gamma * Q_sa[i]
+
+        # train the network with the new values calculated with Q-learning and get loss of our network for evaluation
+        outputs = model(inputs)
+        loss = F.mse_loss(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        for param in model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+
+    queue.append((curr_state, action, reward, next_state, is_terminal))
+    if len(queue) > exp_replay_memory:
+        queue.popleft()
+
+    curr_state = next_state
+    steps += 1
+
+    if steps % timesteps_to_save_weights == 0:
+        torch.save(model.state_dict(), './weights_pytorch')
+
+    print("Timestep: %d, Action: %d, Reward: %.2f, Q: %.2f,Loss: %.2f" % (steps, action, reward, Q_sa[-1], loss))
